1ac1590bd3af7f5504a0ebb4399b7bb661da5b3f
Rupesh Sharma <rupeshss001@gmail.com>
2021-09-14 09:04:58 +0530

    Counter-Assignment| Rupesh Sharma| Incerement-Decrement counter

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var countervalue=document.getElementById("disp_val")
 var orgvalue=0
+// This function is for increament the value by 1 
 function increament(){
     orgvalue+=1
     console.log(orgvalue)
     countervalue.innerText=orgvalue
 }
+// this function reset the value or change it to 0
 function reset(){
     orgvalue=0
     console.log(orgvalue)
     countervalue.innerText=orgvalue
 }
+// This function decreament the value by 1 and ensure it will always positive
 function decreament(){
+    /* in this if-else condition it wil check wehter it is positive or not if it
+    if it is positive so it will decrement otherwise 0*/ 
     if(orgvalue>0){
         orgvalue-=1
         console.log(orgvalue)
         countervalue.innerText=orgvalue
     }else{
+        // it call reset() function to make vale 0 
         alert("Cannot go below 0")
         reset()
     }
